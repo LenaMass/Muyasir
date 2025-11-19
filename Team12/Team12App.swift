f76b9fe905baedee772ce9bfe941d841a33cf1b0
Lena <lenasalhuthali@gmail.com>
2025-11-19 15:27:35 +0300

    Initial Commit

---
+++ Team12/Team12App.swift
@@ -0,0 +1,17 @@
+//
+//  Team12App.swift
+//  Team12
+//
+//  Created by Lena Saeed Alhuthali on 28/05/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct Team12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Team12Tests/Team12Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Team12Tests.swift
+//  Team12Tests
+//
+//  Created by Lena Saeed Alhuthali on 28/05/1447 AH.
+//
+
+import Testing
+@testable import Team12
+
+struct Team12Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
